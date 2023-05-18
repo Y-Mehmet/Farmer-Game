d31f085d62ed7454dd68f50130bfe515c94a3023
Mehmet Yalçin <mehmetyalcin0040@gmail.com>
2023-05-18 17:56:26 +0300

    first

---
+++ Pro2Version2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    Coroutine test;
+    bool isFalse=false;
+    void Start()
+    {
+     
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        
+        if(isFalse==false)
+        {
+            DogInstantiate();
+            isFalse=true;
+            StartCoroutine(TestCorouate());
+        }
+        
+           
+        }
+    }
+    void DogInstantiate(){
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+    
+        IEnumerator TestCorouate(){
+             
+     
+        
+     yield return new WaitForSeconds(2f);
+     isFalse=false;
+
+
+    }
+    
+}
